d847bd964a1057a2e81548507407edb3cfa0bdff
poem <644543260@qq.com>
2020-06-03 11:52:51 +0800

    first commit

---
+++ src/main/java/org/poem/GeoFencingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.poem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFencingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoFencingBackendApplication.class, args);
+    }
+
+}
